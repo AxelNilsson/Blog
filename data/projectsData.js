fb6fe45cb9d4451cc818424fa2a5314b5092bb48
Axel Nilsson <axelnilsson+github@protonmail.com>
2023-05-09 21:53:40 +0200

    added base

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,21 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'LiveDay - Event management platform',
+    description: `LiveDay is a event management platform targeted at sports events. It was created and launched in 2023.`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: 'https://liveday.se',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Padelboard - Padel tournament mobile app',
+    description: `Padelboard is a mobile app and a web interface for creating, playing and organizing all kinds of padel tournaments. The platform grew to over 100 000 users and was acquired by market leader MATCHi in 2022 to complete their suite of products.`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://padelboard.app',
+  },
+  {
+    title: 'EtherExchange - First ethereum exchange in the nordics',
+    description: `Work on the platform started in the spring of 2017 with the launch being later in the fall. During the four months of operations we achieved a revenue growth of 33% month-on-month with a combined revenue of over 1 000 000 SEK and a projected revenue for 2018 of 5 000 000 SEK. Later that year there were new regulatory changes which made the business structure unfeasible. We had to shut down operations 4 months after launch due to regulatory changes and lack of funding.`,
+    imgSrc: '/static/images/google.png',
+    href: '/blog/etherexchange',
   },
 ]
 
--- pages/projects.js
+++ pages/projects.js
@@ -3,17 +3,17 @@ import projectsData from '@/data/projectsData'
 import Card from '@/components/Card'
 import { PageSEO } from '@/components/SEO'
 
-export default function Projects() {
+export default function Companies() {
   return (
     <>
-      <PageSEO title={`Projects - ${siteMetadata.author}`} description={siteMetadata.description} />
+      <PageSEO title={`Companies - ${siteMetadata.author}`} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Projects
+            Companies
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            Featured below are companies that I founded or joined as first employee (CTO).
           </p>
         </div>
         <div className="container py-12">
